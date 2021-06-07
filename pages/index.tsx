6105871fdeabdd7f6febb2f4118ef2284d2ae2ce
Filip Jerga <jerga99@gmail.com>
2021-06-07 10:12:55 +0100

    Ts configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,16 @@ export default function Home() {
 
   const message: string = "Hello World"
 
+  let a: AddEventListenerOptions;
+  let b: NodeJS.Process
+
+  let person: Person = {
+    name: "Filip"
+  }
+
+  // console.log(b.browser)
+
+
   return (
     <div>
       {message}
---
+++ test.d.ts
@@ -0,0 +1,5 @@
+
+
+interface Person {
+  name: string
+}
