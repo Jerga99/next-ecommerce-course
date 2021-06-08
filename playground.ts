f432f74f8069e2acd32e71943caae8206a057692
Filip Jerga <jerga99@gmail.com>
2021-06-08 14:44:01 +0100

    [ai] function params

--- playground.ts
+++ playground.ts
@@ -3,5 +3,16 @@
 
 
 export default function play() {
-  console.log("Hello World")
+  const name: string = "Filip"
+  const age: number = 30
+
+  function logPersonInfo(personName: string, personAge: number) {
+    // const info = "Name: " + personName + ", age: " + personAge
+    const info = `Name: ${personName}, age: ${personAge}`
+    console.log(info)
+    return info
+  }
+
+  logPersonInfo(name, age)
+
 }
