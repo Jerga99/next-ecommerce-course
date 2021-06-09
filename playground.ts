61f448a7a2e521f45b3eebe7ba28e1599191e537
Filip Jerga <jerga99@gmail.com>
2021-06-09 10:56:21 +0100

    [ai] extending type

--- playground.ts
+++ playground.ts
@@ -13,24 +13,23 @@ interface AcademicPerson extends Person {
   publications: string[]
 }
 
+type Car = {
+  name: string
+}
 
+type RaceCar = {
+  speed: number
+} & Car & { mileage: number}
 
 
 export default function play() {
-  const person: AcademicPerson = {
-    name: "filip",
-    age: 23,
-    publications: ["1", "2"]
+  const car: RaceCar = {
+    name: "my car",
+    speed: 100,
+    mileage: 200
   }
 
-  const person2: BusinessPerson = {
-    name: "filip",
-    age: 23,
-    salary: 1000
-  }
+  function logCar(car: Car) {}
 
-  function logPerson(person: Person) {}
-
-
-  logPerson(person2)
+  logCar(car)
 }
