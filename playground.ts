bf0eb052858031f7ab384923e9073a0be1ac25ad
Filip Jerga <jerga99@gmail.com>
2021-06-11 14:54:34 +0100

    [ai] Multiple generic params

--- playground.ts
+++ playground.ts
@@ -5,10 +5,20 @@ interface Person {
   grades: number[]
 }
 
-type PersonKey = keyof Person
+type Logger<
+  FP = string,
+  SP = number,
+  RT = string
+  > = (param1: FP, param2: SP) => RT
 
 export default function play() {
 
-  const personKey: keyof Person = "grades"
+  const superLogger: Logger<string, number, string> = (name, age) => {
+    return "Hello World!"
+  }
+
+  const logger: Logger = (brand, age) => {
+    return brand + age
+  }
 
 }
