254c39b2b95e7449c8421fc8e5fad8664ef15d14
Filip Jerga <jerga99@gmail.com>
2021-06-11 12:14:35 +0100

    [ai] Default values of generics

--- playground.ts
+++ playground.ts
@@ -19,11 +19,14 @@ class BussinessPerson implements Person {
   salary = 1000
 }
 
-interface Car {
+type Car = {
+  brand: string
+  age: number
   name: string
+  city: string
 }
 
-class Logger<T extends Person> {
+class Logger<T extends Person = Car> {
   log(items: Array<T>, callback: (i: T) => void) {
     items.forEach((item) => {
       callback(item)
@@ -33,9 +36,9 @@ class Logger<T extends Person> {
 
 export default function play() {
 
-  const logger = new Logger<BussinessPerson>()
+  const logger = new Logger()
 
-  const persons = [{name: "filip", age: 30, city: ""}, {name: "john", age: 25, city: ""}]
+  const persons = [{name: "filip", age: 30, city: "", brand: ""}, {name: "john", age: 25, city: "", brand: ""}]
   logger.log(persons, (person) => {
     console.log(person)
   })
