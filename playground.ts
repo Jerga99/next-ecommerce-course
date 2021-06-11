4a9e3f60ff404fe965d36a9c1f5a7a5ca5df0ab6
Filip Jerga <jerga99@gmail.com>
2021-06-11 12:07:33 +0100

    [ai] Generic extends

--- playground.ts
+++ playground.ts
@@ -3,9 +3,27 @@
 interface Person {
   name: string
   age: number
+  city: string
 }
 
-class Logger<T> {
+class Student implements Person {
+  name = ""
+  age = 0
+  city = ""
+}
+
+class BussinessPerson implements Person {
+  name = ""
+  age = 0
+  city = ""
+  salary = 1000
+}
+
+interface Car {
+  name: string
+}
+
+class Logger<T extends Person> {
   log(items: Array<T>, callback: (i: T) => void) {
     items.forEach((item) => {
       callback(item)
@@ -15,25 +33,11 @@ class Logger<T> {
 
 export default function play() {
 
-  const logger = new Logger<string>()
+  const logger = new Logger<BussinessPerson>()
 
-  const cars = ["audi", "skoda", "citroen"]
-  logger.log(cars, (car) => {
-    console.log(car)
-  })
-
-  const logger2 = new Logger<number>()
-
-  const numbers = [1,2,3,4]
-  logger2.log(numbers, (num) => {
-    console.log(num)
-  })
-
-  const logger3 = new Logger<Person>()
-
-  const persons = [{name: "filip", age: 30}, {name: "john", age: 25}]
-  logger3.log(persons, (num) => {
-    console.log(num)
+  const persons = [{name: "filip", age: 30, city: ""}, {name: "john", age: 25, city: ""}]
+  logger.log(persons, (person) => {
+    console.log(person)
   })
 
 }
