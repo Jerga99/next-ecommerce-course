92a6f3ffa0c125b1965a6330d7c5dd128f7ceed9
Filip Jerga <jerga99@gmail.com>
2021-06-11 13:24:15 +0100

    [ai] Index array types

--- playground.ts
+++ playground.ts
@@ -3,15 +3,15 @@ interface Person {
   name: string
 }
 
-type SingleType<T> = T extends any[] ? T[number] : unknown
+type CustomArray<T> = {
+  [index: number]: T
+}
 
 
 export default function play() {
 
-  type Type1 = SingleType<string[]>
-  type Type2 = SingleType<number[]>
-  type Type3 = SingleType<Person>
+  const items: CustomArray<string> = ["1", "2", "3"]
+  const items2: CustomArray<number> = [1,2,3]
 
 
-  type Type4 = string[][number]
 }
