161c004263d486d61f35e88520ffe7ed8b78f230
Filip Jerga <jerga99@gmail.com>
2021-06-11 13:38:32 +0100

    [ai] Mapped types

--- playground.ts
+++ playground.ts
@@ -7,11 +7,21 @@ type CustomArray<T> = {
   [index: number]: T
 }
 
+type CustomObject<T = string | number | Person> = {
+  [key: string]: T
+}
+
 
 export default function play() {
 
-  const items: CustomArray<string> = ["1", "2", "3"]
-  const items2: CustomArray<number> = [1,2,3]
+  const person: CustomObject = {
+    "age": 23,
+    "name": "asdasd",
+    "city": "sadasdasd",
+    person: {
+      name: "Filip"
+    }
+  }
 
 
 }
