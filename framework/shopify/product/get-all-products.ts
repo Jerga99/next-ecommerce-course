0f182861abdb9df301e333a0441e1a653fa89fa5
Filip Jerga <jerga99@gmail.com>
2021-06-15 10:02:33 +0100

    Map product from connection type

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -11,9 +11,11 @@ type ReturnType = {
 const getAllProducts = async (): Promise<any> => {
   const { data } = await fetchApi<ReturnType>({query: getAllProductQuery})
 
-  // normalize and return new data!
+  const products = data.products.edges.map(({ node: product }) => {
+    return product
+  }) ?? []
 
-  return data.products
+  return products
 }
 
 export default getAllProducts
