bd81a3ae86f68878f1cef61ad488cabe2b1075ec
Filip Jerga <jerga99@gmail.com>
2021-06-16 12:10:30 +0100

    Next config file

---
+++ framework/shopify/next.config.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+module.exports = {
+  test: "shopify-unique-settings"
+}
---
+++ next.config.js
@@ -0,0 +1,12 @@
+
+
+
+
+module.exports = {
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US"
+  }
+}
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2))
