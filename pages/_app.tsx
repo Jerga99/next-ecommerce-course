2ea93f8c4883bbebf01e50d1223489f236448508
Filip Jerga <jerga99@gmail.com>
2021-06-16 15:12:19 +0100

    _app component

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+
+import { AppProps } from "next/app"
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
