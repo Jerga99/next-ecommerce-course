dce7717d117c59e256a87513d0496e18cb292526
Filip Jerga <jerga99@gmail.com>
2021-06-16 12:47:40 +0100

    Ts config specific setup

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,13 +1,27 @@
 
 
 const path = require("path")
+const fs = require("fs")
 const merge = require("deepmerge")
 
 function withFrameworkConfig(defaultConfig = {}) {
-  const framework = "bigcommerce"
+  const framework = "shopify"
 
   const frameworkNextConfig = require(path.join("../", framework, "next.config"))
   const config = merge(defaultConfig, frameworkNextConfig)
+
+  const tsPath = path.join(process.cwd(), "tsconfig.json")
+  const tsConfig = require(tsPath)
+
+  tsConfig.compilerOptions.paths["@framework"] = [`framework/${framework}`]
+  tsConfig.compilerOptions.paths["@framework/*"] = [`framework/${framework}/*`]
+
+  fs.writeFileSync(
+    tsPath,
+    JSON.stringify(
+      tsConfig, null, 2
+    )
+  )
 
   return config
 }
