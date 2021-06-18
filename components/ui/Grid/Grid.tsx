0e383763aa8cde0aeae9a64bbe6ce1d884a1217d
Filip Jerga <jerga99@gmail.com>
2021-06-18 09:46:50 +0100

    Grid stylings

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,10 +1,11 @@
 
 import { FC, ReactNode } from "react"
+import s from "./Grid.module.css"
 
 const Grid: FC<ReactNode> = ({children}) => {
 
   return (
-    <div className="root-grid">
+    <div className={s.root}>
       {children}
     </div>
   )
