70a5cc95284fc8cb7cee4cbab3be9dce6f54b9cd
Filip Jerga <jerga99@gmail.com>
2021-06-18 09:14:34 +0100

    More product info

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,14 +1,36 @@
 import { Product } from "@common/types/product"
 import { FC } from "react"
+import Link from "next/link"
+import Image from "next/image"
 
 interface Props {
   product: Product
 }
-
+const placeholderImage = "/product-image-placeholder.svg"
 const ProductCard: FC<Props> = ({product}) => {
 
   return (
-    <div>{product.name}</div>
+    <Link href={`/products/${product.slug}`}>
+      <a>
+        <div>
+          <h3>
+            <span>{product.name}</span>
+          </h3>
+          <span>14 $</span>
+        </div>
+        { product.images && (
+          <Image
+            alt={product.name ?? "Product image"}
+            src={placeholderImage}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )
+        }
+      </a>
+    </Link>
   )
 }
 
