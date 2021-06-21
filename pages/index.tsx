bfdeef6facbeae611aee66345f15823d1c9b358b
Filip Jerga <jerga99@gmail.com>
2021-06-21 11:08:52 +0100

    Layout B

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,6 +45,14 @@ export default function Home({
           />
         )}
       </Marquee>
+      <Grid layout="B">
+        { products.slice(0,3).map(product =>
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+      )}
+      </Grid>
     </>
   )
 }
