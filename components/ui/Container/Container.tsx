4a8c798f49b6fdc37a6ccc9c013ec2edb71bac95
Filip Jerga <jerga99@gmail.com>
2021-06-21 09:18:48 +0100

    El type

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -1,16 +1,17 @@
-import { ReactNode, FC } from "react";
+import { ReactNode, FC, ComponentType, HTMLAttributes } from "react";
 
 
 interface Props {
   children: ReactNode | ReactNode[]
-  el?: any
+  el?: ComponentType<HTMLAttributes<HTMLElement>>
 }
 
 
 const Container: FC<Props> = ({children, el: Component = "div"}) => {
 
   return (
-    <Component className="px-6 mx-auto max-w-8xl">
+    <Component
+      className="px-6 mx-auto max-w-8xl">
       {children}
     </Component>
   )
