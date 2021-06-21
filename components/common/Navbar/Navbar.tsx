35a8708278157a3f352389ce943ef9d826522ee5
Filip Jerga <jerga99@gmail.com>
2021-06-21 11:44:46 +0100

    Navbar Links

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -2,13 +2,35 @@
 
 import { FC } from "react"
 import { Container } from "@components/ui"
+import Link from "next/link"
+import s from "./Navbar.module.css"
 
 const Navbar: FC = () => {
 
   return (
     <Container>
-      <div className="flex flex-row md:py-6">
-        <h1>NEXT_STORE</h1>
+      <div className={s.root}>
+        <div className="flex flex-1 items-center">
+          <Link href="/">
+            <a className={s.logo}>
+              NEXT_STORE
+            </a>
+          </Link>
+          <nav className="ml-6 space-x-6">
+            <Link href="/">
+              <a className={s.link}>All</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Clothes</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Accesories</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Shoes</a>
+            </Link>
+          </nav>
+        </div>
       </div>
     </Container>
   )
