505449bc5b0d771e446f31fe3ef1232ae36f513d
Filip Jerga <jerga99@gmail.com>
2021-06-21 11:03:23 +0100

    React ticker

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,5 +1,6 @@
 
 import { ReactNode, FC } from "react"
+import Ticker from "react-ticker"
 import s from "./Marquee.module.css"
 
 interface Props {
@@ -11,9 +12,13 @@ const Marquee: FC<Props> = ({children}) => {
 
   return (
     <div className={s.root}>
-      <div className={s.container}>
-        {children}
-      </div>
+      <Ticker offset={80}>
+        { () =>
+          <div className={s.container}>
+            {children}
+          </div>
+        }
+      </Ticker>
     </div>
   )
 }
