075fb4e3431186d27d38c70f315b75d482dc86bf
Filip Jerga <jerga99@gmail.com>
2021-06-21 09:46:47 +0100

    Flex hero

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -18,7 +18,7 @@ const Hero: FC<Props> = ({headline, description}) => {
           <h2 className={s.headline}>
             {headline}
           </h2>
-          <div>
+          <div className="flex-1 max-w-4xl">
             <p className={s.description}>
               {description}
             </p>
