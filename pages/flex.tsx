f5ba6428a48f4616d7c27bda1d6a1b4d67b5d058
Filip Jerga <jerga99@gmail.com>
2021-06-23 08:57:33 +0100

    Flex Styles

---
+++ pages/flex.tsx
@@ -0,0 +1,17 @@
+
+import s from "./flex.module.css"
+
+const Flex = () => {
+
+  return (
+    <div className={s.parent}>
+      <div className={s.child}></div>
+      <div className={s.child}></div>
+      <div className={s.child}></div>
+      <div className={s.child}></div>
+    </div>
+  )
+}
+
+
+export default Flex
