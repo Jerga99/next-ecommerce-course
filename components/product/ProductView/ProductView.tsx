b7dc35be3404a423b9ee1d036f22c1d1d5176a46
Filip Jerga <jerga99@gmail.com>
2021-06-29 11:03:19 +0100

    Active choice

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -56,19 +56,23 @@ const ProductView: FC<Props> = ({ product }) => {
               <div key={option.id} className="pb-4">
                 <h2 className="uppercase font-medium">{option.displayName}</h2>
                 <div className="flex flex-row py-4">
-                  { option.values.map(optValue =>
-                    <Swatch
-                      key={`${option.id}-${optValue.label}`}
-                      label={optValue.label}
-                      color={optValue.hexColor}
-                      variant={option.displayName}
-                      onClick={() => {
-                        setChoices({
-                          ...choices,
-                          [option.displayName.toLowerCase()]: optValue.label.toLowerCase()
-                        })
-                      }}
-                    />
+                  { option.values.map(optValue => {
+                    const activeChoice = choices[option.displayName.toLowerCase()]
+                    return (
+                      <Swatch
+                        key={`${option.id}-${optValue.label}`}
+                        label={optValue.label}
+                        color={optValue.hexColor}
+                        variant={option.displayName}
+                        active={optValue.label.toLowerCase() === activeChoice}
+                        onClick={() => {
+                          setChoices({
+                            ...choices,
+                            [option.displayName.toLowerCase()]: optValue.label.toLowerCase()
+                          })
+                        }}
+                      />
+                    )}
                   )}
                 </div>
               </div>
--- components/product/Swatch/Swatch.tsx
+++ components/product/Swatch/Swatch.tsx
@@ -2,32 +2,45 @@
 import { FC } from "react"
 import s from "./Swatch.module.css"
 import { Check } from '@components/icons'
+import cn from "classnames"
 
 interface Props {
   color?: string
   label?: string
+  active?: boolean
   variant?: "size" | "color" | string
   onClick: () => void
 }
 
 
 const Swatch: FC<Props> = ({
-  color, label, variant,
+  color, label, variant, active,
   ...rest
 }) => {
 
   label = label?.toLowerCase()
   variant = variant?.toLocaleLowerCase()
 
+  const rootClassName = cn(
+    s.root,
+    {
+      [s.active]: active,
+      [s.color]: color,
+      [s.size]: variant === "size"
+    }
+  )
+
   return (
     <button
       style={color ? {backgroundColor: color} : {}}
-      className={s.root}
+      className={rootClassName}
       {...rest}
     >
-      {/* <span>
-        <Check />
-      </span> */}
+      { variant === "color" && active &&
+        <span>
+          <Check />
+        </span>
+      }
       { variant === "size" ? label : null }
     </button>
   )
