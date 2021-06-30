1bfed7c099b633e3baac7699e8e7f0c9f6b05470
Filip Jerga <jerga99@gmail.com>
2021-06-30 11:52:08 +0100

    use hook handler

--- framework/common/cart/use-add-item.ts
+++ framework/common/cart/use-add-item.ts
@@ -1,13 +1,9 @@
 
+import { handler } from "@framework/cart/use-add-item"
 
 
 const useAddItem = () => {
-
-  return (input: any) => {
-    return {
-      output: JSON.stringify(input) + "_MODIFIED"
-    }
-  }
+  return handler.useHook()
 }
 
 export default useAddItem
--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -3,3 +3,17 @@
 import { useAddItem } from "@common/cart"
 
 export default useAddItem
+
+
+export const handler = {
+  fetcher: () => {
+    console.log("Fetching Data!")
+  },
+  useHook: () => {
+    return (input: any) => {
+      return {
+        output: JSON.stringify(input) + "_MODIFIED"
+      }
+    }
+  }
+}
