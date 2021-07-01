9a3614b3cf3d3e313de6122dfca47795147ca2a8
Filip Jerga <jerga99@gmail.com>
2021-07-01 09:46:48 +0100

    Hook fetcher types

--- framework/common/types/hooks.ts
+++ framework/common/types/hooks.ts
@@ -1,11 +1,16 @@
-
+import { ApiFetcher } from "./api"
 
 export type MutationHookContext = {
   fetch: (input: any) => any
 }
 
+export type FetcherHookContext = {
+  input: any
+  fetch: ApiFetcher
+}
+
 export type MutationHook = {
-  fetcher: (input: any) => any,
+  fetcher: (context: FetcherHookContext) => any,
   useHook(
     context: MutationHookContext
   ): (input: any) => any
--- framework/common/utils/use-hook.ts
+++ framework/common/utils/use-hook.ts
@@ -18,8 +18,10 @@ export const useMutationHook = (
     fetch: (input: any) => {
       return hook.fetcher({
         input,
-        fetch: (input: any) => {
-          return JSON.stringify(input) + "_MODIFIED"
+        fetch: async (input: any) => {
+          return {
+            data: JSON.stringify(input) + "_MODIFIED"
+          }
         }
       })
     }
