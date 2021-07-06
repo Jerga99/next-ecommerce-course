4286bac7efa534af2b1d5e2816882df4c5703e06
Filip Jerga <jerga99@gmail.com>
2021-07-06 09:49:52 +0100

    Display items in the cart

--- components/cart/CartSidebar/CartSidebar.tsx
+++ components/cart/CartSidebar/CartSidebar.tsx
@@ -3,13 +3,13 @@ import { Bag, Cross } from '@components/icons'
 import cn from "classnames"
 import { useUI } from '@components/ui/context'
 import useCart from '@framework/cart/use-cart'
+import { LineItem } from '@common/types/cart'
 
 const CartSidebar: FC = () => {
-  const isEmpty = true
   const { closeSidebar} = useUI()
   const { data } = useCart()
-  console.log(data)
-  debugger
+
+  const isEmpty = (data?.lineItems.length ?? 0) <= 0
 
   const rootClass = cn(
     "h-full flex flex-col",
@@ -51,7 +51,11 @@ const CartSidebar: FC = () => {
             My Cart
           </h2>
           <ul className="py-6 space-y-6 sm:py-0 sm:space-y-0 sm:divide-y sm:divide-accents-3 border-t border-accents-3">
-            Cart Items Here!
+            { data?.lineItems.map((item: LineItem) =>
+              <div key={item.id}>
+                {item.name} - {item.quantity}
+              </div>
+            )}
           </ul>
         </div>
         <div className="flex-shrink-0 px-4  py-5 sm:px-6">
@@ -59,7 +63,7 @@ const CartSidebar: FC = () => {
             <ul className="py-3">
             <li className="flex justify-between py-1">
                 <span>Subtotal</span>
-                <span>20$</span>
+                <span>{data?.lineItemsSubtotalPrice} {data?.currency.code}</span>
               </li>
               <li className="flex justify-between py-1">
                 <span>Taxes</span>
@@ -72,7 +76,7 @@ const CartSidebar: FC = () => {
             </ul>
             <div className="flex justify-between border-t border-accents-3 py-3 font-bold mb-10">
               <span>Total</span>
-              <span>120$</span>
+              <span>{data?.totalPrice} {data?.currency.code}</span>
             </div>
           </div>
           <button
