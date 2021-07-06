d7f647f4db4b831fa2c508259627854f65df90f0
Filip Jerga <jerga99@gmail.com>
2021-07-06 09:10:51 +0100

    Fetcher output description

--- framework/shopify/cart/use-cart.ts
+++ framework/shopify/cart/use-cart.ts
@@ -2,6 +2,7 @@
 import useCart from "@common/cart/use-cart"
 import { Cart } from "@common/types/cart"
 import { SWRHook } from "@common/types/hooks"
+import { Checkout } from "@framework/schema"
 import {
   checkoutToCart,
   createCheckout,
@@ -13,7 +14,9 @@ export type UseCartHookDescriptor = {
   fetcherInput: {
     checkoutId: string
   }
-  fetcherOutput: any
+  fetcherOutput: {
+    node: Checkout
+  }
   data: Cart
 }
 
@@ -29,7 +32,7 @@ export const handler: SWRHook<UseCartHookDescriptor> = {
     options,
     input: { checkoutId }
   }) {
-    let checkout
+    let checkout: Checkout
 
     if (checkoutId) {
       const { data } = await fetch({
@@ -40,7 +43,7 @@ export const handler: SWRHook<UseCartHookDescriptor> = {
       })
       checkout = data.node
     } else {
-      checkout = await createCheckout(fetch)
+      checkout = await createCheckout(fetch as any)
     }
 
     const cart = checkoutToCart(checkout)
--- framework/shopify/utils/create-checkout.ts
+++ framework/shopify/utils/create-checkout.ts
@@ -8,12 +8,17 @@ import { checkoutCreateMutation } from "./mutations"
 
 const createCheckout = async (
   fetch: ApiFetcher<{checkoutCreate: CheckoutCreatePayload}>
-): Promise<Maybe<Checkout | undefined>> => {
+): Promise<Checkout> => {
   const { data } = await fetch({
     query: checkoutCreateMutation
   })
 
   const { checkout } = data.checkoutCreate
+
+  if (!checkout) {
+    throw new Error("Checkout cannot be created!")
+  }
+
   const checkoutId = checkout?.id
 
   if (checkoutId) {
