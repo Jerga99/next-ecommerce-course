29e7ae3c7b6046c5e5100f5c6d6a55d90934db4c
Filip Jerga <jerga99@gmail.com>
2021-07-07 09:04:25 +0100

    Display items counts

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -3,17 +3,27 @@ import s from "./Usernav.module.css"
 import Link from "next/link"
 import { Bag as Cart, Heart } from "@components/icons"
 import { useUI } from "@components/ui/context"
-import useCart from "@common/cart/use-cart"
+import useCart from "@framework/cart/use-cart"
+import { LineItem } from "@common/types/cart"
 
 const Usernav: FC = () => {
   const { openSidebar } = useUI()
   const { data } = useCart()
+
+  const itemsCount = data?.lineItems.reduce((count: number, item: LineItem) => {
+    return count + item.quantity
+  }, 0) ?? 0
 
   return (
     <nav>
       <ul className={s.list}>
         <li className={s.item}>
           <Cart onClick={openSidebar}/>
+          { itemsCount > 0 &&
+            <span className={s.bagCount}>
+              { itemsCount }
+            </span>
+          }
         </li>
         <li className={s.item}>
           <Link href="/wishlist">
